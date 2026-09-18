c87cfbf6ff8744dab1adeadad54951c25d2ed0af
vishnu vardhan <ugs23113_aids.venkat@cbit.org.in>
2026-09-19 02:12:58 +0530

    [328] [Time Beats: 100.00%] [Memory Beats: 76.65%] - LeetPush

---
+++ 328-Odd-Even-Linked-List.py
@@ -0,0 +1,19 @@
+class Solution:
+    def oddEvenList(self, head: ListNode | None) -> ListNode | None:
+        if head is None or head.next is None:
+            return head
+
+        odd = head
+        even = head.next
+        even_head = even
+
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+
+            even.next = even.next.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
